e8e8e3bb53718fd3af5006aaeb7d65677cf30d27
LeonardoCHb <leonardohably@hotmail.com>
2020-09-22 18:33:00 -0300

    quantity/price global fix

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -48,18 +48,22 @@ const Cart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     const total = products.reduce((accumulator, product) => {
-      const productsSub = product.price * product.quantity
+      const productsTotal = product.price * product.quantity
 
-      return accumulator + productsSub
+      return accumulator + productsTotal
     }, 0)
 
     return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity
 
-    return 0;
+      return accumulator + productsQuantity
+    }, 0)
+
+    return total;
   }, [products]);
 
   return (
